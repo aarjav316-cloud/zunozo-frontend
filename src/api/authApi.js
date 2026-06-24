4617869f289d061b5644648388bccca6739f6f1b
Aarjav jain <aarjav316@gmail.com>
2026-06-24 15:55:49 +0530

    axios setup done

---
+++ src/api/authApi.js
@@ -0,0 +1,126 @@
+import axiosInstance from "./axios";
+
+export const signupUser = async (data) => {
+  try {
+    const response = await axiosInstance.post("/auth/register", data);
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const verifyOtp = async (data) => {
+  try {
+    const response = await axiosInstance.post("/auth/verify-otp", data);
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const signinUser = async (data) => {
+  try {
+    const response = await axiosInstance.post("/auth/login", data);
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const forgotPassword = async (data) => {
+  try {
+    const response = await axiosInstance.post("/auth/forgot-password", data);
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const resetPassword = async (data) => {
+  try {
+    const response = await axiosInstance.post("/auth/reset-password", data);
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const logoutUser = async () => {
+  try {
+    const response = await axiosInstance.post("/auth/logout");
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const response = await axiosInstance.get("/auth/me");
+    return response.data;
+  } catch (error) {
+    throw (
+      error.response?.data || {
+        success: false,
+        message: "Something went wrong",
+      }
+    );
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
